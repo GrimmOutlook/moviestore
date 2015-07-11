320383456ba5cddf4d785773a09527e0f904aa46
GrimmOutlook <grimmoutlook87@gmail.com>
2015-07-11 00:43:28 -0400

    devise_for routes & registration view modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
   root 'movies#index'
 
   resources :movies, only: [:show, :new, :index]
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
