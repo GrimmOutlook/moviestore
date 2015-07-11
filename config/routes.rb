bb4c93f88bd384da518994e56998b12fbb575612
GrimmOutlook <grimmoutlook87@gmail.com>
2015-07-10 21:28:56 -0400

    Installed Devise, created User model, created Devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   root 'movies#index'
+
   resources :movies, only: [:show, :new, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
